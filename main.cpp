5493824a67e6fc51e787dedbd005ff121bff32dc
lukecavabarrett <luca.cavallery@gmail.com>
2021-02-09 23:50:41 +0000

    mtx::matrix instead of C vector

--- main.cpp
+++ main.cpp
@@ -2,81 +2,43 @@
 #include <vector>
 #include <cmath>
 #include <fstream>
+#include <algorithm>
+#include "matrix.hpp"
 
 static constexpr size_t L = 1000;
-static constexpr size_t T = 10000;
-
-bool instable(int* M){
-    for (int i=0; i<L*L; ++i){
-        if (M[i]>3) return true;
-    }
-    return false;
-}
-
-int overloaded(int* M){
-    for (int i=0; i<L*L; ++i){
-        if (M[i]>3) return i;
-    }
-    return -1;
-}
+static constexpr size_t T = 1000;
 
 int main() {
 
-    int M[L*L];
+  mtx::matrix<uint32_t, L, L> M;
 
-    for (int i=0; i<L*L; ++i){
-        M[i]=0;
+  std::fill(M.flat().begin(), M.flat().end(), 0);
+
+  auto overloaded_it =
+      [&M]() { return std::find_if(M.flat().begin(), M.flat().end(), [](uint32_t x) { return x > 3; }); };
+  for (size_t t = 0; t < T; ++t) {
+
+    fprintf(stderr, "\r %03.3f%%", float(t)*100 / T);
+    M[L / 2][L / 2]++; // increment central cell
+
+    for (uint32_t *it = overloaded_it(); it != M.flat().end(); it = overloaded_it()) {
+      // while there is an overloaded, share load with neighbours
+      auto[y, x] = M.index_of(it);
+      M[y][x] -= 4;
+      if (y)M[y - 1][x]++;
+      if (y < L - 1)M[y + 1][x]++;
+      if (x)M[y][x - 1]++;
+      if (x < L - 1)M[y][x + 1]++;
     }
 
-    FILE *fp = fopen("M", "w");
+  }
+  fprintf(stderr, "\r%lu epochs completed\n", T);
 
-    for (int t = 0; t < T; ++t) {
+//  std::ofstream output("M2.ascii");
+//  for (int k = 0; k < (L * L); k++) {
+//    output << M[k] << " "; // behaves like cout - cout is also a stream
+//    if ((k + 1) % L == 0)output << "\t" << std::endl;
+//  }
 
-        std::cout << t << std::endl;
-        M[L*L/2 + L/2] = M[L*L/2 +L/2] + 1;
-
-        /*while ( instable(M) ) {
-
-            std::vector Indexes=overloaded(M);
-            int randomIndex = Indexes[rand()%size(Indexes)];
-
-            M[randomIndex] = M[randomIndex] - 4;
-
-            if (randomIndex != L-1 ) M[randomIndex+1] = M[randomIndex+1] + 1;
-            if (randomIndex != 0 )   M[randomIndex-1] = M[randomIndex-1] + 1;
-            if (randomIndex < L*L - L ) M[randomIndex+L] = M[randomIndex+L] + 1;
-            if (randomIndex > L-1 ) M[randomIndex-L] = M[randomIndex-L] + 1;
-
-        }*/
-
-        while ( overloaded(M)!=-1 ) {
-
-            int randomIndex = overloaded(M);
-            M[randomIndex] = M[randomIndex] - 4;
-
-            if (randomIndex != L-1 ) M[randomIndex+1] = M[randomIndex+1] + 1;
-            if (randomIndex != 0 )   M[randomIndex-1] = M[randomIndex-1] + 1;
-            if (randomIndex < L*L - L ) M[randomIndex+L] = M[randomIndex+L] + 1;
-            if (randomIndex > L-1 ) M[randomIndex-L] = M[randomIndex-L] + 1;
-
-        }
-    }
-
-
-    /*for (int i = 1; i < L*L; ++i) {
-        if (i%L == 0) {
-            fprintf(fp, "\n %d", M[i]);
-        } else {
-            fprintf(fp, "\t %d", M[i]);
-        }
-    }*/
-
-    std::ofstream output("M2.ascii");
-    for (int k=0; k<(L*L); k++)
-    {
-            output << M[k] << " "; // behaves like cout - cout is also a stream
-        if ((k+1)%L==0)output << "\t" << std::endl;
-    }
-
-    return 0;
+  return 0;
 }
