b6ec5ecff06e9cd27fcb2dc30b97330647df3f2d
fcavalleri <fcavalleri@studenti.uninsubria.it>
2021-02-10 00:01:26 +0100

    chiamo solo l'indice del primo sito > 3 che trovo

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <cmath>
 #include <fstream>
 
-#define L 300
-#define T 50000
+#define L 1000
+#define T 10000
 
 bool instable(int* M){
     for (int i=0; i<L*L; ++i){
@@ -13,12 +13,19 @@ bool instable(int* M){
     return false;
 }
 
-std::vector<int> overloaded(int* M){
+/*std::vector<int> overloaded(int* M){
     std::vector<int> Index;
     for (int i=0; i<L*L; ++i){
         if (M[i]>3) Index.push_back(i);
     }
     return Index;
+}*/
+
+int overloaded(int* M){
+    for (int i=0; i<L*L; ++i){
+        if (M[i]>3) return i;
+    }
+    return -1;
 }
 
 int main() {
@@ -36,7 +43,7 @@ int main() {
         std::cout << t << std::endl;
         M[L*L/2 + L/2] = M[L*L/2 +L/2] + 1;
 
-        while ( instable(M) ) {
+        /*while ( instable(M) ) {
 
             std::vector Indexes=overloaded(M);
             int randomIndex = Indexes[rand()%size(Indexes)];
@@ -48,8 +55,21 @@ int main() {
             if (randomIndex < L*L - L ) M[randomIndex+L] = M[randomIndex+L] + 1;
             if (randomIndex > L-1 ) M[randomIndex-L] = M[randomIndex-L] + 1;
 
+        }*/
+
+        while ( overloaded(M)!=-1 ) {
+
+            int randomIndex = overloaded(M);
+            M[randomIndex] = M[randomIndex] - 4;
+
+            if (randomIndex != L-1 ) M[randomIndex+1] = M[randomIndex+1] + 1;
+            if (randomIndex != 0 )   M[randomIndex-1] = M[randomIndex-1] + 1;
+            if (randomIndex < L*L - L ) M[randomIndex+L] = M[randomIndex+L] + 1;
+            if (randomIndex > L-1 ) M[randomIndex-L] = M[randomIndex-L] + 1;
+
         }
     }
+
 
     /*for (int i = 1; i < L*L; ++i) {
         if (i%L == 0) {
@@ -60,13 +80,11 @@ int main() {
     }*/
 
     std::ofstream output("M2.ascii");
-    for (int k=1; k<(L*L); k++)
+    for (int k=0; k<(L*L); k++)
     {
             output << M[k] << " "; // behaves like cout - cout is also a stream
-        if (k%L==0)output << "\t" << std::endl;
+        if ((k+1)%L==0)output << "\t" << std::endl;
     }
-
-    output << 0 << " ";
 
     return 0;
 }
