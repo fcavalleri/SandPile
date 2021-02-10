eb38bf40fcb978e1dee7edb1e90c2adf248c5886
lukecavabarrett <luca.cavallery@gmail.com>
2021-02-10 00:26:41 +0000

    fast version

--- main.cpp
+++ main.cpp
@@ -4,31 +4,56 @@
 #include <fstream>
 #include <algorithm>
 #include "matrix.hpp"
+#include "udcs.h"
 
 static constexpr size_t L = 1000;
-static constexpr size_t T = 1000;
+static constexpr size_t T = 10000000;
 
 int main() {
 
   mtx::matrix<uint32_t, L, L> M;
+  mtx::udcs<L * L, uint32_t> overloaded;
 
   std::fill(M.flat().begin(), M.flat().end(), 0);
 
-  auto overloaded_it =
-      [&M]() { return std::find_if(M.flat().begin(), M.flat().end(), [](uint32_t x) { return x > 3; }); };
   for (size_t t = 0; t < T; ++t) {
 
-    fprintf(stderr, "\r %03.3f%%", float(t)*100 / T);
-    M[L / 2][L / 2]++; // increment central cell
+    //print the progress bar
+    {
+      static constexpr size_t print_every = 1000;
+      static size_t ip = 0;
+      ++ip;
+      if (ip >= print_every) {
+        fprintf(stderr, "\r %03.3f%%", float(t+1) * 100 / T);
+        ip = 0;
+      }
+    }
 
-    for (uint32_t *it = overloaded_it(); it != M.flat().end(); it = overloaded_it()) {
+    M[L / 2][L / 2]++; // increment central cell
+    if(M[L/2][L/2]>3)overloaded.insert((L/2)*L + L/2);
+
+    while (!overloaded.empty()) {
+      size_t yx = *overloaded.begin();
       // while there is an overloaded, share load with neighbours
-      auto[y, x] = M.index_of(it);
+      auto[y, x] = M.index_of(&M.flat()[yx]);
       M[y][x] -= 4;
-      if (y)M[y - 1][x]++;
-      if (y < L - 1)M[y + 1][x]++;
-      if (x)M[y][x - 1]++;
-      if (x < L - 1)M[y][x + 1]++;
+      if (M.flat()[yx] < 4)overloaded.erase(yx);
+      if (y) {
+        M[y - 1][x]++;
+        if (M.flat()[yx - L] > 3)overloaded.insert(yx - L);
+      }
+      if (y < L - 1) {
+        M[y + 1][x]++;
+        if (M.flat()[yx + L] > 3)overloaded.insert(yx + L);
+      }
+      if (x) {
+        M[y][x - 1]++;
+        if (M.flat()[yx - 1] > 3)overloaded.insert(yx - 1);
+      }
+      if (x < L - 1) {
+        M[y][x + 1]++;
+        if (M.flat()[yx + 1] > 3)overloaded.insert(yx + 1);
+      }
     }
 
   }
@@ -39,6 +64,12 @@ int main() {
 //    output << M[k] << " "; // behaves like cout - cout is also a stream
 //    if ((k + 1) % L == 0)output << "\t" << std::endl;
 //  }
+  for (size_t y = 0; y < L; ++y) {
+    for (size_t x = 0; x < L; ++x) {
+      printf("%d ", M[y][x]);
+    }
+    printf("\t\n");
+  }
 
   return 0;
 }
--- udcs.h
+++ udcs.h
@@ -3,63 +3,69 @@
 
 #include <algorithm>
 #include <numeric>
+namespace mtx {
+namespace detail {
+template<typename T>
+struct next_t {};
+template<>
+struct next_t<uint64_t> { typedef uint32_t type; };
+template<>
+struct next_t<uint32_t> { typedef uint16_t type; };
+template<>
+struct next_t<uint16_t> { typedef uint8_t type; };
 
-namespace detail{
-template <typename T> struct next_t {};
-template <> struct next_t<uint64_t>      { typedef uint32_t type; };
-template <> struct next_t<uint32_t>       { typedef uint16_t type; };
-template <> struct next_t<uint16_t>     { typedef uint8_t type; };
-
-template <typename Max_t, Max_t upto_n, typename Best_t=Max_t, typename Try_t=uint64_t, bool try_is_better = (sizeof(Try_t) <= sizeof(Best_t) && upto_n == Max_t(Try_t(upto_n)))>
+template<typename Max_t, Max_t upto_n, typename Best_t=Max_t, typename Try_t=uint64_t, bool try_is_better = (
+    sizeof(Try_t) <= sizeof(Best_t) && upto_n == Max_t(Try_t(upto_n)))>
 struct tight_int {
   typedef typename tight_int<Max_t, upto_n, Best_t, typename next_t<Try_t>::type>::type type;
 };
 
-template <typename Max_t, Max_t upto_n, typename Best_t, typename Try_t>
-struct tight_int<Max_t, upto_n, Best_t, Try_t, true>  {
+template<typename Max_t, Max_t upto_n, typename Best_t, typename Try_t>
+struct tight_int<Max_t, upto_n, Best_t, Try_t, true> {
   typedef typename tight_int<Max_t, upto_n, Try_t, typename next_t<Try_t>::type>::type type;
 };
 
-template <typename Max_t, Max_t upto_n, typename Best_t>
-struct tight_int<Max_t, upto_n, Best_t, uint8_t, true>  {
+template<typename Max_t, Max_t upto_n, typename Best_t>
+struct tight_int<Max_t, upto_n, Best_t, uint8_t, true> {
   typedef uint8_t type;
 };
 
-template <typename Max_t, Max_t upto_n, typename Best_t>
+template<typename Max_t, Max_t upto_n, typename Best_t>
 struct tight_int<Max_t, upto_n, Best_t, uint8_t, false> {
   typedef Best_t type;
 };
 }
 
-template<size_t N, typename T = typename detail::tight_int<size_t,N>::type  >
-class unsigned_dat_compact_set{
+template<size_t N, typename T = typename detail::tight_int<size_t, N>::type>
+class udcs {
+  // unsigned_dat_compact_set
 public:
-  unsigned_dat_compact_set(T size=0) : ssize(size) {
-    std::iota(content.begin(),content.end(),0);
-    std::iota(content_map.begin(),content_map.end(),0);
+  udcs(T size = 0) : ssize(size) {
+    std::iota(content.begin(), content.end(), 0);
+    std::iota(content_map.begin(), content_map.end(), 0);
   }
-  void clear() {ssize = 0;}
-  void fill() {ssize = N;}
+  void clear() { ssize = 0; }
+  void fill() { ssize = N; }
   bool insert(T x) {
-    if(size_t x_idx = content_map.at(x); x_idx < ssize) {
+    if (size_t x_idx = content_map.at(x); x_idx < ssize) {
       return false; //already in
     } else {
       T y = content.at(ssize);
-      std::swap(content[ssize],content[x_idx]);
-      std::swap(content_map[x],content_map[y]);
+      std::swap(content[ssize], content[x_idx]);
+      std::swap(content_map[x], content_map[y]);
       ++ssize;
       return true;
     }
   }
-  size_t size() const {return ssize;}
+  size_t size() const { return ssize; }
   auto begin() const { return content.cbegin(); }
-  auto end() const { return content.cbegin()+ssize; }
+  auto end() const { return content.cbegin() + ssize; }
   bool erase(size_t x) {
-    if(size_t x_idx = content_map.at(x); x_idx < ssize) {
+    if (size_t x_idx = content_map.at(x); x_idx < ssize) {
       --ssize;
       T y = content.at(ssize);
-      std::swap(content[ssize],content[x_idx]);
-      std::swap(content_map[x],content_map[y]);
+      std::swap(content[ssize], content[x_idx]);
+      std::swap(content_map[x], content_map[y]);
       return true;
     } else {
       return false; //already out
@@ -68,10 +74,11 @@ public:
   bool contains(size_t x) const {
     return content_map.at(x) < ssize;
   }
-  bool empty() const {return ssize==0;}
+  bool empty() const { return ssize == 0; }
 private:
   T ssize;
-  std::array<T,N> content, content_map;
+  std::array<T, N> content, content_map;
 };
+}
 
 #endif //SANDPILE__UDCS_H_
